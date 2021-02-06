082dba581612b0c0e1ac6d9747605fbaa938e56d
Faizoo <faizaan3424@gmail.com>
2021-02-06 17:45:07 -0500

    deposit finished

--- behind_the_counter.cpp
+++ behind_the_counter.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <cstdlib>
 #include <string>
-using namespace std;
-double balance;
+double balance = 0;
 
 void deposit() {
-
+    int deposit;
+    std::cout << "\nHow much would you like to deposit? ";
+    std::cin >> deposit;
+    balance += deposit;
+    std::cout << "\n$" << deposit << " has been deposited to your account.";
 }
 
 void withdraw() {
--- user_helper.cpp
+++ user_helper.cpp
@@ -1,12 +1,13 @@
 #include "behind_the_counter.cpp"
+using namespace std;
 
 string getUserAction() {
     string userAction;
     while (true) {
-        std::cout << "\nWelcome to the best C++ Bank! Would you like to deposit, withdraw, or check your account? ";
+        std::cout << "\nWelcome to the best C++ Bank! Would you like to quit, deposit, withdraw, or check your account? ";
         std::cin >> userAction;
-        if(userAction != "deposit" && userAction != "withdraw" && userAction != "check") {
-            std::cout << "\nSorry, but that's not a valid option. Please choose either deposit, withdraw, or check!\n";
+        if(userAction != "deposit" && userAction != "withdraw" && userAction != "check" && userAction !="quit") {
+            std::cout << "\nSorry, but that's not a valid option. Please choose either quit, deposit, withdraw, or check!\n";
             continue;
         }
         return userAction;
@@ -14,9 +15,13 @@ string getUserAction() {
 }
 
 int main() {
-    string userAction{ getUserAction() };
-    if (userAction == "deposit") deposit();
-    else if (userAction == "withdraw") withdraw();
-    else check();
+    while (true) {
+        string userAction{ getUserAction() };
+        if (userAction == "deposit") deposit();
+        else if (userAction == "withdraw") withdraw();
+        else if (userAction == "check") check();
+        else break;
+    }
+    std::cout << "\nHope to see you again!";
     return EXIT_SUCCESS;
 }
