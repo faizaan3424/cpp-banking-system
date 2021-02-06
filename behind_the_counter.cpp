5c9ae8cf432fa7ef74195ab7e6a564de8bd57aab
Faizoo <faizaan3424@gmail.com>
2021-02-06 17:18:04 -0500

    base commit

---
+++ behind_the_counter.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <cstdlib>
+
+void test() {
+    std::cout<<"meme review";
+}
---
+++ user_helper.cpp
@@ -0,0 +1,6 @@
+#include "behind_the_counter.cpp"
+
+int main() {
+    test();
+    return EXIT_SUCCESS;
+}
